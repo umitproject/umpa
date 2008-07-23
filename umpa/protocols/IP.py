12d2db401403e6964fe9ca6f85341a01d83fd5d3
Bartosz Skowron <getxsick@gmail.com>
2008-07-23 12:19:51 +0000

    Renamed protocols/base.py to _.py to get it private.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@3192 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umpa/protocols/IP.py
+++ umpa/protocols/IP.py
@@ -19,7 +19,7 @@
 # along with this library; if not, write to the Free Software Foundation, 
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 
-from base import *
+from _ import *
 from umpa.utils.my_exceptions import UMPAAttributeException
 
 class HIHL(IntField):
