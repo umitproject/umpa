cad678cd9c6ebc3a6ca386b11c48e368cf2492f5
Bartosz Skowron <getxsick@gmail.com>
2008-08-18 17:22:41 +0000

    Added setup.py and some text files like COPYING etc. Also removed symlink from examples/umpa
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@3541 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2008 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+import glob
+import os.path
+from distutils.core import setup
+
+UMPA_VERSION = '0.1'
+
+setup(  name            = "UMPA",
+        version         = UMPA_VERSION,
+        description     = "Umit Manipulation of Packets Art",
+        author          = "Bartosz SKOWRON",
+        author_email    = "getxsick@gmail.com",
+        url             = "http://trac.umitproject.org/wiki/PacketManipulator/BackEnd",
+        license         = "GNU LGPLv2",
+        platforms       = ["Platform Independent"],
+        packages        = [ "umpa",
+                            "umpa.protocols",
+                            "umpa.extensions",
+                            "umpa.utils",
+                            ],
+        data_files      = [ (os.path.join('share','umpa','examples'), glob.glob("examples/*"), )]
+)
