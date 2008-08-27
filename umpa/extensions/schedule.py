8a8c05c3bc54dc4949ba8c62c87b539f835eb2de
Bartosz Skowron <getxsick@gmail.com>
2008-08-27 14:56:58 +0000

    Documentation fixes
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@3647 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umpa/extensions/schedule.py
+++ umpa/extensions/schedule.py
@@ -20,7 +20,7 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 
 """
-Schedule feature for the sending packets.
+Schedule feature is for sending packets.
 
 2 features are provided:
  1. delay before start sending
