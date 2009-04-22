5eff10f03a7998e9e94871d3bbbeceea7b85a330
Bartosz Skowron <getxsick@gmail.com>
2009-04-22 18:25:17 +0000

    tests: add umpa.utils.bits.get_bits()
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@4552 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ tests/unit/test_utils/test_bits.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+import py.test
+from umpa.utils.bits import *
+
+class TestUtilBitsGet(object):
+    def test_get_bits(self):
+        assert get_bits(10, 2) == 2
+        assert get_bits(666666, 3) == 5
+        assert get_bits(666666, 3, 1) == 2
+        assert get_bits(666666, 3, rev_offset=True) == 2
+        assert get_bits(999999999999999, 9) == 454
+        assert get_bits(999999999999999, 9, 9) == 382
+        assert get_bits(999999999999999, 9, 9, True) == 319
+
+    def test_get_bits_errors(self):
+        py.test.raises(ValueError, get_bits, 0, 1)
+        py.test.raises(ValueError, get_bits, 10, 100)
