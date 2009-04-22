567fffb2b98bc423df97a539c2f33a51afcdc520
Bartosz Skowron <getxsick@gmail.com>
2009-04-22 23:18:15 +0000

    add coverage.py tool to analyze tests modify run_tests.sh script to handle with it - now it shows statistics also option '-m' (need to be pass as a first argument!) gives extra stats
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@4557 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+cur=`pwd`
+cd /tmp
+
+rm -f .coverage 2>/dev/null
+if [ $? -eq 1 ]; then
+	echo "#### ERROR ####"
+	echo "Can't remove /tmp/.coverage"
+	echo "Please remove it manually first"
+	echo "#### ERROR ####"
+	exit 1
+fi
+
+m=""
+if [ $# -gt 0 ]  && [ $1 == "-m" ]; then
+	m=$1
+	shift
+fi
 
 echo "#### TESTS FOR UMPA [ START ] ####"
-py/bin/py.test $* tests
+$cur/tests/coverage.py -x $cur/py/bin/py.test $* $cur/tests
 echo "#### TESTS FOR UMPA [ FINISH ] ####"
+
+echo "#### STATS FOR UMPA [ START ] ####"
+$cur/tests/coverage.py -r $m --omit=/usr/lib,$cur/tests,$cur/py
+echo "#### STATS FOR UMPA [ FINISH ] ####"
+cd $cur
