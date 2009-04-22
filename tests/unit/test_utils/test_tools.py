5e1587bc5998db86cf0f15dd43784766b079a79d
Bartosz Skowron <getxsick@gmail.com>
2009-04-22 17:43:52 +0000

    tests: umpa.utils.tools
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@4550 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ tests/unit/test_utils/test_tools.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+from umpa.utils.tools import *
+
+class TestUtilsTools(object):
+    def test_dict_sequence(self):
+        l = (1, 2, 3, 4)
+        assert dict_from_sequence(l) == {1:2, 3:4}
+
+        l = (1, 2, 3, 4, 5)
+        assert dict_from_sequence(l) == {1:2, 3:4}
+
+        l = [1, 2, 3, 4]
+        assert dict_from_sequence(l) == {1:2, 3:4}
+
+        l = [1, 2, 3, 4, 5]
+        assert dict_from_sequence(l) == {1:2, 3:4}
+
+        l = []
+        assert dict_from_sequence(l) == {}
+
+        l = [1]
+        assert dict_from_sequence(l) == {}
