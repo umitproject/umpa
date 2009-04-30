331d086139e8e071143f65672d0ee80bb3fe59df
Bartosz Skowron <getxsick@gmail.com>
2009-04-30 01:05:02 +0000

    tests: IP protocol. they are skipped for the moment because we use semi-IP protocol (see #310 comment for more info) some work have to be done first. unfortunatelly
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@4634 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ tests/unit/test_protocols/test_ip.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+import py.test
+
+from umpa.protocols import IP
+
+class TestIP(object):
+    def test_get_raw(self):
+        py.test.skip('auto fields for IP have to implemented '
+                    'first (like Identification)')
+        # TODO more tests would be nice
+        # this test pre/post raw methods as well
+        ip = IP(source_address='127.0.0.1', destination_address='127.0.0.1')
+
+        assert ip._raw(0, 0, [ip], 0) == \
+                (0x450000149aa900004000e23e7f0000017f000001, 160)
