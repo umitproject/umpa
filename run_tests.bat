e3b858cdb0d1c339afa2490325a8711647d5fe5b
Bartosz Skowron <getxsick@gmail.com>
2009-04-30 13:59:20 +0000

    rebuild batch script disable coverage feature - it catchs too many files and i don't know why the same problem under Linux is resolved by running coverage.py from /tmp here it doesn't work
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@4658 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- run_tests.bat
+++ run_tests.bat
@@ -1,22 +1,28 @@
 @ echo off
 
-cd %temp%
-IF  Exist "coverage". (
-	del /a:R coverage
-)else (
-	echo ERROR !!! Can't remove coverage.
-	exit 1
-)
+mode con: cols=150 lines=50
+cls
 
-IF %1 EQU -m (
-	set m=%1
-)else (
-	set m=""
-)
+rem cd %temp%
+rem IF  Exist "coverage". (
+rem 	del /a:R coverage
+rem )else (
+rem 	echo ERROR !!! Can't remove coverage.
+rem	exit 1
+rem )
+
+rem IF %1 EQU -m (
+rem 	set m=%1
+rem )else (
+rem 	set m=""
+rem )
 
 echo "#### TESTS FOR UMPA [ START ] ####"
-%cur%/tests/coverage.py -x %cur%/py/bin/py.test %* %cur%/tests
+rem %cur%/tests/coverage.py -x %cur%/py/bin/py.test %* %cur%/tests
+py\bin\win32\py.test tests
 echo "#### TESTS FOR UMPA [ FINISH ] ####"
-%cur%/tests/coverage.py -r %m --omit=/usr/lib,%cur%/tests,%cur%/py 
-echo "#### STATS FOR UMPA [ FINISH ] ####"
-cd %cur%
+
+rem echo "#### STATS FOR UMPA [ START ] ####"
+rem %cur%/tests/coverage.py -r %m --omit=/usr/lib,%cur%/tests,%cur%/py 
+rem echo "#### STATS FOR UMPA [ FINISH ] ####"
+rem cd %cur%
