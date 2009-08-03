e5d528e7843d3ceb101e15100029314180a935d7
Bartosz Skowron <getxsick@gmail.com>
2009-08-03 19:15:37 +0000

    React mechanism. Currently supports reverting ports/hosts.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/UMPA@5150 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ tests/a_unit/test_extensions/test_models.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+import Queue
+import threading
+
+import umpa
+import umpa.sniffing
+from umpa.protocols import IP, TCP
+from umpa.extensions import models
+from umpa.utils.exceptions import UMPAException
+from tests.utils import SendPacket
+
+import py.test
+
+class SniffThread(threading.Thread):
+    def __init__(self, filter, device, queue):
+        super(SniffThread, self).__init__()
+        self._filter = filter
+        self._device = device
+        self._queue = queue
+
+class RevPortsThread(SniffThread):
+    def run(self):
+        pkt = umpa.sniffing.sniff(2, device=self._device, filter=self._filter)
+        try:
+            assert pkt[0].ip.src == "127.0.0.1"
+            assert pkt[0].ip.dst == "127.0.0.1"
+            assert pkt[0].tcp.srcport == 80
+            assert pkt[0].tcp.dstport == 0
+            assert pkt[1].ip.src == "127.0.0.1"
+            assert pkt[1].ip.dst == "127.0.0.1"
+            assert pkt[1].tcp.srcport == 0
+            assert pkt[1].tcp.dstport == 80
+        except AssertionError, e:
+            self._queue.put(e)
+
+class RevHostsThread(SniffThread):
+    def run(self):
+        pkt = umpa.sniffing.sniff(2, device=self._device, filter=self._filter)
+        try:
+            assert pkt[0].ip.src == "67.205.14.183"
+            assert pkt[0].ip.dst == "127.0.0.1"
+            assert pkt[1].ip.src == "127.0.0.1"
+            assert pkt[1].ip.dst == "67.205.14.183"
+        except AssertionError, e:
+            self._queue.put(e)
+
+class TestModels(object):
+    pass
+
+class TestReact(TestModels):
+    def test_revports(self):
+        # use queue to communicate between threads
+        # py.test doesn't catch assertions from threads by itself
+        queue = Queue.Queue()
+
+        th = SendPacket(umpa.Packet(IP(), TCP(srcport=80, dstport=0)))
+        th.start()
+        
+        th2 = RevPortsThread("host 127.0.0.1 and port 80", "lo", queue)
+        th2.start()
+        models.react(1, filter="host 127.0.0.1 and port 80", device="lo",
+                    revports=True)
+        th.join()
+        th2.join()
+        if not queue.empty():
+            err = queue.get()
+            raise AssertionError(err)
+        queue.join()
+
+    def test_revhosts(self):
+        # use queue to communicate between threads
+        # py.test doesn't catch assertions from threads by itself
+        queue = Queue.Queue()
+
+        th = SendPacket(umpa.Packet(IP(src="67.205.14.183", dst="127.0.0.1"), strict=False))
+        th.start()
+        
+        th2 = RevHostsThread("host 67.205.14.183", "any", queue)
+        th2.start()
+        models.react(1, filter="host 67.205.14.183", device="any", revhosts=True)
+        th.join()
+        th2.join()
+        if not queue.empty():
+            err = queue.get()
+            raise AssertionError(err)
+        queue.join()
+        
+    def test_wrongargs(self):
+        py.test.raises(UMPAException, "models.react(1, foo=True)")
+        py.test.raises(UMPAException,
+                "models.react(1, foo=True, revports=True)")
+        py.test.raises(UMPAException,
+                "models.react(1, foo=False, revhosts=True)")
+        py.test.raises(UMPAException, "models.react(1, foo=True, bar=False)")
---
+++ umpa/extensions/models.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Adriano Monteiro Marques.
+#
+# Author: Bartosz SKOWRON <getxsick at gmail dot com>
+#
+# This library is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; either version 2.1 of the License, or 
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library; if not, write to the Free Software Foundation, 
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+"""
+Useful functions to make life easier.
+
+This extension doesn't provide anything new what you can't do by your own.
+But it provides some functions which are used often so using it can decrease
+time for coding.
+"""
+
+import umpa
+from umpa.sniffing import sniff_next
+from umpa.utils.exceptions import UMPAException
+
+def react(count, filter=None, device=None, timeout=0, snaplen=1024, promisc=True, **kwargs):
+    """
+    Sniff and resend packets with some reaction.
+
+    Sniffed packet can be modify (e.g. by reversing src/dst ports) and resend.
+
+    For description of arguments please see umpa.sniffing package.
+    Available reactions:
+     1. revhosts
+     2. revports
+    To set reactions use dict-style keywords like revhosts=True.
+    """
+
+    def revhosts(pkt):
+        tmp = pkt.ip.dst
+        pkt.ip.dst = pkt.ip.src
+        pkt.ip.src = tmp
+        return pkt
+
+    def revports(pkt):
+        tmp = pkt.tcp.dstport
+        pkt.tcp.dstport = pkt.tcp.srcport
+        pkt.tcp.srcport = tmp
+        return pkt
+
+    avail_opts = ('revports', 'revhosts',)
+
+    for opt in kwargs:
+        if opt not in avail_opts:
+            raise UMPAException("Undefined option " + opt)
+
+    options = {}
+    for opt in avail_opts:
+        options[opt] = kwargs.get(opt)
+
+    
+    sock = umpa.Socket()
+    for i in xrange(count):
+        pkt = sniff_next(filter=filter, device=device, timeout=timeout,
+                        snaplen=snaplen, promisc=promisc)
+        for opt in options:
+            if options[opt] is True:
+                pkt = locals()[opt](pkt)
+        # remove 2nd layer
+        while pkt.protos[0].layer < 3:
+            pkt.protos.pop(0)
+        sock.send(pkt)
